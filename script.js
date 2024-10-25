c926d47ec31bfefbed7ccaae2681d50c3f318f0b
DhruvRoyale <dhruvgidwani5@gmail.com>
2024-10-25 16:56:09 +0530

    Test event listener

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ function requestOrientationPermission(){
     DeviceOrientationEvent.requestPermission()
     .then(response => {
         if (response == 'granted') {
-            window.addEventListener('deviceorientation', (e) => {
-                document.querySelector('#test').innerHTML = e;
+            screen.orientation.addEventListener("change", (e) => {
+                let type = e.target.type;
+                let angle = e.target.angle;
+
+                document.querySelector('#test').innerHTML = "Type: " + type + "\n Angle: " + angle;
             })
         }
     })
